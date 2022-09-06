df024e456bc1934b95d01fa6451e945bb22075da
Kavanaugh Frank <kf106921@ohio.edu>
2022-09-06 17:21:08 -0400

    Git Lab Program

---
+++ git-lab-program.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc , char const *argv[]){
+    //Kavanaugh Frank 
+    //Date - 9/6/22
+    cout << "Hello GIT!!" << endl;
+}
